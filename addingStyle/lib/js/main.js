a53d02bd1df2bd32fdb0a6d13bb6b42c507f885b
Michael <michaelalvarado@uagateway.org>
2018-07-31 10:04:41 -0400

    finnished 3 projects

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,4 @@
+document.body.style.backgroundColor = "#d3c26f";
+
+document.getElementsByClassName("name")[0].style.textShadow = "5px 5px 1px yellow";
+document.getElementsByClassName("name")[1].style.textShadow = "5px 5px 1px yellow";
---
+++ theBrush/lib/js/main.js
@@ -0,0 +1,6 @@
+function onClick(){
+   alert("You've been deleted")
+   document.querySelector("img").src = "assets/rengar.png";
+}
+
+document.addEventListener('click', onClick)
